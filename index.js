878cfac2a93ff98cdd040926849484bab6c78b69
zoe <zoeedge0703@gmail.com>
2023-07-17 12:25:43 -0500

    added the startApplication function for the CLI

--- index.js
+++ index.js
@@ -12,3 +12,50 @@ connection.connect((err) => {
     console.log('Connected!');
     startApplication();
 });
+
+function startApplication() {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'list',
+            message: 'Please choose an action below.',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ]
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case 'View all Departments':
+                    viewAllDepartments();
+                    break;
+                case 'View all Roles':
+                    viewAllRoles();
+                    break;
+                case 'View all Employees':
+                    viewAllEmployees();
+                    break;
+                case 'Add a Department':
+                    addDepartment();
+                    break;            
+                case 'Add a Role':
+                    addRole();
+                    break;            
+                case 'Add an Employee':                    
+                    addEmployee();                    
+                    break;
+                case 'Update an Employee Role':
+                    updateEmployeeRole();
+                    break;
+                case 'Exit':
+                    connection.end();
+                    break;
+            }
+        });
+}
